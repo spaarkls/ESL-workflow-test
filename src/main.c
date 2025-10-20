cb2f6c3ac4dacb6e792b02a6a89e938240142c39
spaarkls <ds.baranof@yandex.ru>
2025-10-20 20:37:15 +0300

    Implement setters and getters for the vector3D
    Close #3

--- src/main.c
+++ src/main.c
@@ -7,54 +7,66 @@
 
 START_TEST(test_sum)
 {
-    vector3D_t a = {1, 2, 3};
-    vector3D_t b = {4, 5, 6};
-    vector3D_t res;
+    
+    vector3D_t *a = vector3D_init_with_value(1, 2, 3);
+    vector3D_t *b = vector3D_init_with_value(4, 5, 6);
+    vector3D_t *res = vector3D_init();
 
-    vector3D_sum(&a, &b, &res);
+    vector3D_sum(a, b, res);
 
-    ck_assert_double_eq_tol(res.x, 5.0, EPS);
-    ck_assert_double_eq_tol(res.y, 7.0, EPS);
-    ck_assert_double_eq_tol(res.z, 9.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_x(res), 5.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_y(res), 7.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_z(res), 9.0, EPS);
+    vector3D_destroy(a);
+    vector3D_destroy(b);
+    vector3D_destroy(res);
+
 }
 END_TEST
 
 START_TEST(test_sub)
 {
-    vector3D_t a = {4, 5, 6};
-    vector3D_t b = {1, 2, 3};
-    vector3D_t res;
+    vector3D_t *a = vector3D_init_with_value(4, 5, 6);
+    vector3D_t *b = vector3D_init_with_value(1, 2, 3);
+    vector3D_t *res = vector3D_init();
 
-    vector3D_sub(&a, &b, &res);
+    vector3D_sub(a, b, res);
 
-    ck_assert_double_eq_tol(res.x, 3.0, EPS);
-    ck_assert_double_eq_tol(res.y, 3.0, EPS);
-    ck_assert_double_eq_tol(res.z, 3.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_x(res), 3.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_y(res), 3.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_z(res), 3.0, EPS);
+    vector3D_destroy(a);
+    vector3D_destroy(b);
+    vector3D_destroy(res);
 }
 END_TEST
 
 START_TEST(test_dot)
 {
-    vector3D_t a = {1, 2, 3};
-    vector3D_t b = {4, 5, 6};
+    vector3D_t *a = vector3D_init_with_value(1, 2, 3);
+    vector3D_t *b = vector3D_init_with_value(4, 5, 6);
 
-    double res = vector3D_dot(&a, &b);
-
+    double res = vector3D_dot(a, b);
     ck_assert_double_eq_tol(res, 32.0, EPS);
+    vector3D_destroy(a);
+    vector3D_destroy(b);
 }
 END_TEST
 
 START_TEST(test_cross)
 {
-    vector3D_t a = {1, 2, 3};
-    vector3D_t b = {4, 5, 6};
-    vector3D_t res;
+    vector3D_t *a = vector3D_init_with_value(1, 2, 3);
+    vector3D_t *b = vector3D_init_with_value(4, 5, 6);
+    vector3D_t *res = vector3D_init();
 
-    vector3D_cross(&a, &b, &res);
+    vector3D_cross(a, b, res);
 
-    ck_assert_double_eq_tol(res.x, -3.0, EPS);
-    ck_assert_double_eq_tol(res.y,  6.0, EPS);
-    ck_assert_double_eq_tol(res.z, -3.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_x(res), -3.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_y(res),  6.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_z(res), -3.0, EPS);
+    vector3D_destroy(a);
+    vector3D_destroy(b);
+    vector3D_destroy(res);
 }
 END_TEST
 
@@ -62,95 +74,92 @@ START_TEST(test_init_destroy)
 {
     vector3D_t *v = vector3D_init();
     ck_assert_ptr_nonnull(v);
-
-    v->x = 10;
-    v->y = 20;
-    v->z = 30;
-
-    ck_assert_double_eq_tol(v->x, 10, EPS);
-    ck_assert_double_eq_tol(v->y, 20, EPS);
-    ck_assert_double_eq_tol(v->z, 30, EPS);
-
     vector3D_destroy(v);
 }
 END_TEST
 
 START_TEST(test_sum_with_zero)
 {
-    vector3D_t a = {3, -2, 5};
-    vector3D_t zero = {0, 0, 0};
-    vector3D_t res;
+    vector3D_t *a = vector3D_init_with_value(3, -2, 5);
+    vector3D_t *zero = vector3D_init_with_value(0, 0, 0);
+    vector3D_t *res = vector3D_init();
 
-    vector3D_sum(&a, &zero, &res);
-    ck_assert_double_eq_tol(res.x, 3, EPS);
-    ck_assert_double_eq_tol(res.y, -2, EPS);
-    ck_assert_double_eq_tol(res.z, 5, EPS);
+    vector3D_sum(a, zero, res);
+    ck_assert_double_eq_tol(vector3D_get_x(res), 3, EPS);
+    ck_assert_double_eq_tol(vector3D_get_y(res), -2, EPS);
+    ck_assert_double_eq_tol(vector3D_get_z(res), 5, EPS);
+    vector3D_destroy(a);
+    vector3D_destroy(zero);
+    vector3D_destroy(res);
 }
 END_TEST
 
-START_TEST(test_sum_commutative)
+START_TEST(test_dot_2)
 {
-    vector3D_t a = {1, 2, 3};
-    vector3D_t b = {4, 5, 6};
-    vector3D_t res1, res2;
-
-    vector3D_sum(&a, &b, &res1);
-    vector3D_sum(&b, &a, &res2);
-
-    ck_assert_double_eq_tol(res1.x, res2.x, EPS);
-    ck_assert_double_eq_tol(res1.y, res2.y, EPS);
-    ck_assert_double_eq_tol(res1.z, res2.z, EPS);
-}
-END_TEST
-
-START_TEST(test_cross_anticommutative)
-{
-    vector3D_t a = {1, 2, 3};
-    vector3D_t b = {4, 5, 6};
-    vector3D_t ab, ba;
-
-    vector3D_cross(&a, &b, &ab);
-    vector3D_cross(&b, &a, &ba);
-
-    ck_assert_double_eq_tol(ab.x, -ba.x, EPS);
-    ck_assert_double_eq_tol(ab.y, -ba.y, EPS);
-    ck_assert_double_eq_tol(ab.z, -ba.z, EPS);
-}
-END_TEST
-
-START_TEST(test_dot_perpendicular)
-{
-    vector3D_t a = {1, 0, 0};
-    vector3D_t b = {0, 1, 0};
-    double res = vector3D_dot(&a, &b);
+    vector3D_t *a = vector3D_init_with_value(1, 0, 0);
+    vector3D_t *b = vector3D_init_with_value(0, 1, 0);
+    
+    double res = vector3D_dot(a, b);
     ck_assert_double_eq_tol(res, 0.0, EPS);
 }
 END_TEST
 
-START_TEST(test_cross_parallel)
+START_TEST(test_cross_2)
 {
-    vector3D_t a = {1, 2, 3};
-    vector3D_t b = {2, 4, 6};
-    vector3D_t res;
+    vector3D_t *a = vector3D_init_with_value(1, 2, 3);
+    vector3D_t *b = vector3D_init_with_value(2, 4, 6);
+    vector3D_t *res = vector3D_init();
 
-    vector3D_cross(&a, &b, &res);
+    vector3D_cross(a, b, res);
 
-    ck_assert_double_eq_tol(res.x, 0.0, EPS);
-    ck_assert_double_eq_tol(res.y, 0.0, EPS);
-    ck_assert_double_eq_tol(res.z, 0.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_x(res), 0.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_y(res), 0.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_z(res), 0.0, EPS);
+    vector3D_destroy(a);
+    vector3D_destroy(b);
+    vector3D_destroy(res);
 }
 END_TEST
 
 START_TEST(test_null_safety)
 {
-    vector3D_t a = {1, 2, 3};
-    vector3D_t b = {4, 5, 6};
-    vector3D_cross(NULL, &b, &a);
-    vector3D_cross(&a, NULL, &b);
-    vector3D_cross(&a, &b, NULL);
+    vector3D_t *a = vector3D_init_with_value(1, 2, 3);
+    vector3D_t *b = vector3D_init_with_value(4, 5, 6);
+    vector3D_cross(NULL, b, a);
+    vector3D_cross(a, NULL, b);
+    vector3D_cross(a, b, NULL);
+    
+    vector3D_destroy(a);
+    vector3D_destroy(b);
 }
 END_TEST
 
+START_TEST(test_getters) {
+    vector3D_t *a = vector3D_init_with_value(1, 2, 3);
+    ck_assert_double_eq_tol(vector3D_get_x(a), 1.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_y(a), 2.0, EPS);
+    ck_assert_double_eq_tol(vector3D_get_z(a), 3.0, EPS);
+    vector3D_destroy(a);
+}
+END_TEST
+
+START_TEST(test_setters) {
+    vector3D_t *a = vector3D_init();
+    vector3D_set_x(a, 12);
+    ck_assert_double_eq_tol(vector3D_get_x(a), 12, EPS);
+    vector3D_set_y(a, 9321);
+    ck_assert_double_eq_tol(vector3D_get_y(a), 9321, EPS);
+    vector3D_set_z(a, -156);
+    ck_assert_double_eq_tol(vector3D_get_z(a), -156, EPS);
+    
+
+    vector3D_set_xyz(a, 12, 14, 15);
+    ck_assert_double_eq_tol(vector3D_get_x(a), 12, EPS);
+    ck_assert_double_eq_tol(vector3D_get_y(a), 14, EPS);
+    ck_assert_double_eq_tol(vector3D_get_z(a), 15, EPS);
+
+    vector3D_destroy(a);
+}
 
 Suite *vector3D_suite(void)
 {
@@ -167,11 +176,11 @@ Suite *vector3D_suite(void)
     tcase_add_test(tc_core, test_init_destroy);
 
     tcase_add_test(tc_core, test_sum_with_zero);
-    tcase_add_test(tc_core, test_sum_commutative);
-    tcase_add_test(tc_core, test_cross_anticommutative);
-    tcase_add_test(tc_core, test_dot_perpendicular);
-    tcase_add_test(tc_core, test_cross_parallel);
+    tcase_add_test(tc_core, test_dot_2);
+    tcase_add_test(tc_core, test_cross_2);
     tcase_add_test(tc_core, test_null_safety); 
+    tcase_add_test(tc_core, test_getters); 
+    tcase_add_test(tc_core, test_setters); 
     suite_add_tcase(s, tc_core);
 
     return s;
--- src/vector/vector3D.c
+++ src/vector/vector3D.c
@@ -1,6 +1,16 @@
 #include "vector3D.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
+typedef struct vector3D_t {
+    double x, y, z;
+} vector3D_t;
+
+static double handle_null_pointer(const char *msg, double retval) {
+    fprintf(stderr, "%s", msg);
+    return retval;
+}
 
 static void vector3D_mult_number(vector3D_t *res, vector3D_t *vec1, double num) {
     if (!vec1 || !res) { return; }
@@ -16,10 +26,20 @@ static double determinant(double a1, double a2, double b1, double b2) {
 vector3D_t *vector3D_init(void) {
     vector3D_t *res = (vector3D_t *)malloc(sizeof(vector3D_t));
     if (!res) {
-        perror("init:");
-        exit(1);
+        perror("vector3D_init:");
+        return NULL;
     }
     res->x = 0; res->y = 0; res->z = 0;
+    return res;
+}
+
+vector3D_t *vector3D_init_with_value(double x, double y, double z) {
+    vector3D_t *res = (vector3D_t *)malloc(sizeof(vector3D_t));
+    if (!res) {
+        perror("vector3D_init_with_value:");
+        return NULL;
+    }
+    res->x = x; res->y = y; res->z = z;
     return res;
 }
 
@@ -29,34 +49,37 @@ void vector3D_destroy(vector3D_t *vector) {
     }
 }
 
-void vector3D_sum(const vector3D_t *vector1, const vector3D_t *vector2, vector3D_t *res) {
+int vector3D_sum(const vector3D_t *vector1, const vector3D_t *vector2, vector3D_t *res) {
     if (!vector1 || !vector2 || !res) {
-        return;
+        return handle_null_pointer("vector3D_sum: null pointer\n", VECTOR3D_ERROR);
     }
     res->x = vector1->x + vector2->x;
     res->y = vector1->y + vector2->y;
     res->z = vector1->z + vector2->z;
+    return VECTOR3D_OK;
 }
 
-void vector3D_sub(const vector3D_t *vector1, const vector3D_t *vector2, vector3D_t *res) {
+int vector3D_sub(const vector3D_t *vector1, const vector3D_t *vector2, vector3D_t *res) {
     if (!vector1 || !vector2 || !res) {
-        return;
+        return handle_null_pointer("vector3D_sub: null pointer", VECTOR3D_ERROR);
     }
     res->x = vector1->x - vector2->x;
     res->y = vector1->y - vector2->y;
     res->z = vector1->z - vector2->z;
+
+    return VECTOR3D_OK;
 }
 
 double vector3D_dot(const vector3D_t *vector1, const vector3D_t *vector2) {
     if (!vector1 || !vector2) { 
-        return 0.0; 
+        return handle_null_pointer("vector3D_dot: null pointer\n", NAN);
     }
     return vector1->x * vector2->x + vector1->y * vector2->y + vector1->z * vector2->z;
 }
 
-void vector3D_cross(const vector3D_t *vector1, const vector3D_t *vector2, vector3D_t *res) {
+int vector3D_cross(const vector3D_t *vector1, const vector3D_t *vector2, vector3D_t *res) {
     if (!vector1 || !vector2 || !res) {
-        return;
+        return handle_null_pointer("vector3D_cross: null pointer\n", VECTOR3D_ERROR);
     }
     vector3D_t i;
     vector3D_t j;
@@ -75,5 +98,55 @@ void vector3D_cross(const vector3D_t *vector1, const vector3D_t *vector2, vector
     vector3D_mult_number(&k, &k, det3);
     vector3D_sub(&i, &j, res);
     vector3D_sum(res, &k, res);
+    return VECTOR3D_OK;
 }
 
+double vector3D_get_x(const vector3D_t *vector) {
+    if (!vector) return handle_null_pointer("vector3D_get_x: null pointer\n", NAN);
+    return vector->x;
+}
+
+double vector3D_get_y(const vector3D_t *vector) {
+    if (!vector) return handle_null_pointer("vector3D_get_y: null pointer\n", NAN);
+    return vector->y;
+    
+}
+
+double vector3D_get_z(const vector3D_t *vector) {
+    if (!vector) return handle_null_pointer("vector3D_get_z: null pointer\n", NAN);
+    return vector->z;
+}
+
+int vector3D_set_x(vector3D_t *vector, double x) {
+    if (vector) {
+        vector->x = x;
+        return VECTOR3D_OK;
+    }
+    return handle_null_pointer("vector3D_set_x: null pointer\n", VECTOR3D_ERROR);
+}
+
+int vector3D_set_y(vector3D_t *vector, double y) {
+    if (vector) {
+        vector->y = y;
+        return VECTOR3D_OK;
+    }
+    return handle_null_pointer("vector3D_set_y: null pointer\n", VECTOR3D_ERROR);
+}
+
+int vector3D_set_z(vector3D_t *vector, double z) {
+    if (vector) {
+        vector->z = z;
+        return VECTOR3D_OK;
+    }
+    return handle_null_pointer("vector3D_set_z: null pointer\n", VECTOR3D_ERROR);
+}
+
+int vector3D_set_xyz(vector3D_t *vector, double x, double y, double z) {
+    if (vector) {
+        vector->x = x;
+        vector->y = y;
+        vector->z = z;
+        return VECTOR3D_OK;
+    }
+    return handle_null_pointer("vector3D_set_xyz: null pointer\n", VECTOR3D_ERROR);
+}
